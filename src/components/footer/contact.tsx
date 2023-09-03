3f859a4bf27fa0b0cdc6f76d7dfb40a5e2b734ad
TheAveron <77922089+TheAveron@users.noreply.github.com>
2023-09-03 16:41:31 +0200

    feat: adding contact block in footer

---
+++ src/components/footer/contact.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h2>Nous contacter</h2>
+      <p>Si vous avez des questions ou des demandes, n'hésitez pas à nous contacter.</p>
+      <div className="contact-details">
+        <p>Email: informatiqueVDuruy@gmail.com</p>
+        <p>Discord: <a href='https://discord.gg/cGkKnNCh2v'>https://discord.gg/cGkKnNCh2v</a></p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './../../assets/css/common/footer.css'
+import Contact from './contact';
 
 function Footer() {
     return (
         <footer className="footer">
-          <div className='footer-box'><p>
-© 2023 Copyright - Club d'informatique de Victor Duruy - All rights reserved</p></div>
+            <div className='footer-box'>
+                <p>© 2023 Copyright - Club d'informatique de Victor Duruy - All rights reserved</p>
+            </div>
+            <div className='footer-box-2'></div>
+            <div className='footer-box-3'></div>
+            <div className='footer-box-4'></div>
+            <Contact />
         </footer>
-      );
-    }
-    
-    export default Footer;
+    );
+}
+
+export default Footer;
