51bdebc1cb232d5b32a9d15527d2c0b05a034c53
TheAveron <77922089+TheAveron@users.noreply.github.com>
2023-09-19 18:06:55 +0200

    feat: adding aboutus

--- src/components/homepage/aboutus.tsx
+++ src/components/homepage/aboutus.tsx
@@ -4,21 +4,20 @@ import './../../assets/css/pages/home/aboutus.css'
 function AboutUs() {
     return (
         <section id='aboutus' className="about-us">
-            <div  className="about-content">
-                <h2>About Us</h2>
+            <div className="about-content">
+                <h2>À Propos de Nous</h2>
                 <p>
-                    Welcome to our Informatics Club! We are a passionate community of
-                    technology enthusiasts dedicated to exploring the world of computer
-                    science and informatics.
+                    Bienvenue sur le site du club d'Informatique de Victor Duruy ! Le Groupe d'Informatique et de Technologie est bien plus qu'un simple club - c'est un lieu de découverte, d'apprentissage et d'innovation pour les esprits curieux et les amateurs de technologie de notre école. Notre mission est de cultiver une passion pour l'informatique, la technologie et la créativité numérique parmi nos membres.
                 </p>
                 <p>
-                    Our mission is to promote knowledge sharing, organize exciting events,
-                    and foster a love for technology and innovation among our members.
+                    Rejoignez-Nous : Que vous soyez un débutant enthousiaste ou un expert en informatique chevronné, le Groupe d'Informatique et de Technologie vous accueille les bras ouverts. Rejoignez notre communauté dynamique pour explorer, apprendre et grandir dans le monde passionnant de la technologie.
+                </p>
+                <p>
+                    Nous sommes impatients de vous rencontrer et de partager cette aventure passionnante avec vous !
                 </p>
             </div>
         </section>
-        );
-    }
-    
-    export default AboutUs;
-    
+    );
+};
+
+export default AboutUs;
