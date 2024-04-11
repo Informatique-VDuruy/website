c5151a01088b697fd3efde399a9c48605e359b0f
Averon <77922089+TheAveron@users.noreply.github.com>
2024-04-11 10:51:21 +0200

    feat: adding a descritpion for cybersecurity

--- src/components/homepage/planning.tsx
+++ src/components/homepage/planning.tsx
@@ -7,15 +7,17 @@ function Planning() {
                 <div className='Sequence'>
                     <h2>Web developpement</h2>
                     <h3>Du 17 octobre 2023 au 9 janvier 2024</h3>
-                    <p>Au cours de cette première séance, nous allons aborder les bases du développement de sites internet, et découvrir des outils permettant de nous organiser dans le développement.</p>
+                    <p>Au cours de cette première séquence, nous allons aborder les bases du développement de sites internet, et découvrir des outils permettant de nous organiser dans le développement.</p>
                 </div>
                 <div className='Sequence'>
                     <h2>Cybersécurité</h2>
                     <h3>Du 16 janvier 2024 au 2 avril 2024</h3>
+                    <p>Au cours de cette deuxième séquence, nous allons aborder les bases de la cybersécurité, et étudier les principes de la recherche de failles de sécurité.</p>
                 </div>
                 <div className='Sequence'>
                     <h2>Game developpement</h2>
                     <h3>Du 23 avril 2024 au mardi 4 juin 2024</h3>
+                    <p></p>
                 </div>
             </div>
         </section>
